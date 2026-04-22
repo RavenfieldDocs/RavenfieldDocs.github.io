7bba61dc3a2c94d2c5a31b0cae56e53e34f60397
hanz <hi@hanz.dev>
2026-04-23 01:30:11 +0700

    chore: Changed Tutorial to Docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,9 +91,9 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docsSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
